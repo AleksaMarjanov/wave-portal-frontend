5722d0f19f881bbc4dfc486c5b49514522cb607e
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-06-07 20:25:15 -0500

    added bootstrap navbar

--- web3-frontend/components/About.jsx
+++ web3-frontend/components/About.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="about-wrapper">
-      <div className="header">👋 Hey there!</div>
-      <div className="bio">
-        I am Aleksa Front End Developer, really passionate about Web 3 Space!
-        Connect your Ethereum wallet and wave at me!
+    <>
+      <div className="about-wrapper">
+        <div className="header">👋 Hey there!</div>
+        <div className="bio">
+          I am Aleksa Front End Developer, really passionate about Web 3 Space!
+          Connect your Ethereum wallet and wave at me!
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- web3-frontend/src/App.jsx
+++ web3-frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Wave from "../components/Wave";
-import Header from "../components/Header";
 import About from "../components/About";
 import { Routes, Route } from "react-router";
 import "./App.scss";
